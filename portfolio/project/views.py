18ab543f3be6a2140a33c0c6c1feb5bc7f588e88
Gess <artem.fedorkevich@gmail.com>
2021-09-22 18:48:34 +0300

    Second part

---
+++ portfolio/project/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from .models import Project
+from blog.models import Post
+import os
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+
+
+def project_detail(request, pk):
+    if pk==1:
+        project = Project.objects.get(pk=pk)
+        context = {
+            'project': project
+        }
+
+        if request.method == 'POST':
+            os.system('python game_finalfix.py')
+
+        return render(request, 'project_detailbutton.html', context)
+    elif pk==2:
+        project = Project.objects.get(pk=pk)
+        context = {
+            'project': project
+        }
+        posts = Post.objects.all().order_by('-created_on')
+        contextblog = {
+            "posts": posts,
+        }
+        return render(request, 'project_detailswithblogbutton.html', context)
+    elif pk==3:
+        project = Project.objects.get(pk=pk)
+        context = {
+            'project': project
+        }
+        posts = Post.objects.all().order_by('-created_on')
+        contextblog = {
+            "posts": posts,
+        }
+        return render(request, 'project_detailresume.html', context)
+    else:
+        project = Project.objects.get(pk=pk)
+        context = {
+            'project': project
+        }
+
+
+
+        return render(request, 'project_detail.html', context)
+
