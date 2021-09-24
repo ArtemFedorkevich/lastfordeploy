b3d7b9b457f75db2d6fb9f5c396e6edb93b52a45
Gess <artem.fedorkevich@gmail.com>
2021-09-24 16:19:13 +0300

    migrations smalldescriptions

--- portfolio/project/migrations/0001_initial.py
+++ portfolio/project/migrations/0001_initial.py
@@ -17,6 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=100)),
                 ('description', models.TextField()),
+                ('smalldescription', models.TextField()),
                 ('technology', models.CharField(max_length=20)),
                 ('image', models.FileField(upload_to='img/')),
             ],
