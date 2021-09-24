5cfee8dad0863973e0d81a07b089f173756c0571
Gess <artem.fedorkevich@gmail.com>
2021-09-24 16:00:19 +0300

    migrations

--- portfolio/project/models.py
+++ portfolio/project/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    smalldescription = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.FileField(upload_to='img/')
